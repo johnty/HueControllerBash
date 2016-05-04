cc8438892c58a60bdfe6795be9b7a40ea9ba01e8
johnty <johntywang@gmail.com>
2016-05-04 17:56:06 +0000

    simple analog brightness controller for RPi

---
+++ pi/HueAnalog.sh
@@ -0,0 +1,24 @@
+#set brightness of 1st lightbulb according to first analog channel
+# of a mcp3004/8 connected to SPI0
+
+#trap ctrl_c
+trap ctrl_c INT
+
+function ctrl_c() {
+   echo "Goodbyte!"
+
+   exit 0
+}
+echo PiShield Hue Brightness Controller...
+while true;
+do
+   #read Analog ch 0:
+   VAL=`gpio -x mcp3004:100:0 aread 100`
+   BRI=`expr $VAL / 4`
+   #echo BRI = $BRI
+   #set Hue brightness
+   cd .. #hack
+   ./HueControllerBri.sh 1 $BRI &> /dev/null
+   cd pi #hack
+   sleep 0.1
+done
